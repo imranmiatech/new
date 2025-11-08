f48d58e85eabeaab6e3060eb6cbbcf10c0bbf14f
Mdimran19 <imran32472@gmail.com>
2025-11-08 12:28:13 +0600

    first commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const app = express()
 app.use(express.json());
 app.use(cors({
-    origin:['http://localhost:5173', 'http://localhost:5174'],
+    origin:[process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
  
     credentials: true,
